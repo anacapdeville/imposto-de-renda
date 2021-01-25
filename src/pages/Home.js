792b1a7a456f707a89762768d5d7a9efeb56ccf7
Ana Flávia <anaflvia@gmail.com>
2021-01-25 11:03:23 -0300

    estrutura básica do projeto

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <nav>
+        <button type="button">Registrar funcionário</button>
+        <button type="button">Tabelas e cálculos do IRRF</button>
+      </nav>
+    )
+  }
+}
+
+export default Home;
